20e78d8c13878000ce6a3104b31c5576d1ccde05
nikusha882 <nikajalabadze76@gmail.com>
2026-02-10 20:45:24 +0400

    commit

--- level 1/homework/hw.py
+++ level 1/homework/hw.py
@@ -1,11 +1,63 @@
-#შექმენით ცვლადები სადაც შეინახავთ ინფორმაციას თქვენს თავზე მაგ(name , age , address , height , weight) და ტერმინალში გამოიტანეთ სრული ინფორმაცია
-name="ნიკა" #string
-age=14 #integer
-adress="დიდი დიღომი" #string
-height=1.78 #float
-weight=65 #integer
-print(name)
+A=15
+B=5
+print(A+B)
+print(A-B)
+print(A*B)
+print(A/B)
+print(A**B)
+print(A//B)
+
+birth_year=2011
+current_year=2026
+age=birth_year-current_year
 print(age)
-print(adress)
-print(height)
-print(weight)
+
+
+number1 = 10
+number2 = 20
+number3 = 30
+number4 = 40
+number5 = 50
+
+numbers = number1 + number2 + number3 + number4 + number5
+average = numbers / 5
+print(average)
+
+
+# დეკლარაცია ნიშნავს ცვლადის შექმნას (გამოცხადებას)
+# Python-ში ცვლადის დეკლარაცია და ინიციალიზება ერთდროულად ხდება
+
+x = 10
+# ↑ აქ:
+# x — ცვლადის სახელია
+# = — მინიჭების ოპერატორია
+# 10 — მნიშვნელობა
+# ეს არის ინიციალიზება (ცვლადს პირველად მივანიჭეთ მნიშვნელობა)
+
+
+# ინიციალიზება ნიშნავს, რომ ცვლადს ვაძლევთ საწყის მნიშვნელობას
+age = 14
+# age ცვლადი ინიციალიზებულია მნიშვნელობით 25
+
+
+# რეინიციალიზება ნიშნავს უკვე არსებულ ცვლადში ახალი მნიშვნელობის მინიჭებას
+age = 20
+# ↑ აქ age უკვე არსებობდა, მაგრამ მნიშვნელობა შეიცვალა
+# ეს არის რეინიციალიზება
+
+
+# კიდევ ერთი მაგალითი
+number = 5      # ინიციალიზება
+number = 15     # რეინიციალიზება
+
+print(number)   # ტერმინალში გამოვა 15
+
+
+#კონკატენაცია ნიშნავს ორი ან მეტი ტექსტის (string)ის შეერთებას
+name="nika"
+surname="jalabadze"
+full_name=name +" "+ surname
+print(full_name)
+
+
+#debugging ნიშნავს რაიმე შეცდომის შესწორებას
