508f4b44261ee5e392084e62b4ed2ddc399de2ba
nikusha882 <nikajalabadze76@gmail.com>
2026-02-10 20:38:18 +0400

    commit

--- level 1/homework/hw.py
+++ level 1/homework/hw.py
@@ -1,9 +1,9 @@
 #შექმენით ცვლადები სადაც შეინახავთ ინფორმაციას თქვენს თავზე მაგ(name , age , address , height , weight) და ტერმინალში გამოიტანეთ სრული ინფორმაცია
-name="ნიკა"
-age=14
-adress="დიდი დიღომი"
-height=178
-weight=65
+name="ნიკა" #string
+age=14 #integer
+adress="დიდი დიღომი" #string
+height=1.78 #float
+weight=65 #integer
 print(name)
 print(age)
 print(adress)
