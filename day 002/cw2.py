33823ae1f6c7d56e50b6be09b65e251427fdc93e
nikusha882 <nikajalabadze76@gmail.com>
2026-02-28 18:44:48 +0400

    commit

---
+++ day 002/cw2.py
@@ -0,0 +1,7 @@
+# Day 2 Classwork
+word1 ="მე"
+word2 ="მიყვარს"
+word3 ="პროგრამირება"
+word4 ="ძალიან"
+sentence=word1+" "+word2 +" "+ word3 +" "+ word4
+print(sentence)
---
+++ day 004/cw4.py
@@ -0,0 +1,9 @@
+user_name="nika"
+user_age= 14
+user_adress="tbilisi"
+user_hobby="boxing,mma,any other martial arts,basketball"
+
+print(user_name)
+print(user_age)
+print(user_adress)
+print(user_hobby)
